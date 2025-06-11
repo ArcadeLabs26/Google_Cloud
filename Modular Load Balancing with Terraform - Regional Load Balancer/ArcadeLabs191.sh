bd3df410906803e79c3b7d16bdae02088883bc13
Arcade Labs <arcadelabs26@gmail.com>
2025-06-12 01:15:48 +0530

    Add files via upload

---
+++ Modular Load Balancing with Terraform - Regional Load Balancer/ArcadeLabs191.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+
+
+echo
+echo -e "${CYAN}${BOLD_TEXT}==============================================${RESET_FORMAT}"
+echo -e "${YELLOW}${BOLD_TEXT}             Arcade Labs Solution           ${RESET_FORMAT}"
+echo -e "${CYAN}${BOLD_TEXT}==============================================${RESET_FORMAT}"
+echo
+
+
+echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Progress${RESET}"
+
+gcloud auth list
+
+export REGION=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-region])")
+
+gcloud config set compute/region "$REGION"
+
+git clone https://github.com/GoogleCloudPlatform/terraform-google-lb
+cd ~/terraform-google-lb/examples/basic
+
+export GOOGLE_PROJECT=$(gcloud config get-value project)
+
+terraform init
+
+sed -i 's/us-central1/'"$REGION"'/g' variables.tf
+
+echo "$GOOGLE_PROJECT" | terraform plan
+
+echo "$GOOGLE_PROJECT" | terraform apply --auto-approve
+
+EXTERNAL_IP=$(terraform output | grep load_balancer_default_ip | cut -d = -f2 | xargs echo -n)
+
+echo "http://${EXTERNAL_IP}"
+
+echo "${GREEN}${BOLD}Congratulations:- ${RESET}" "${WHITE}${BOLD}On Completing the Lab !!!${RESET}"
+
+
+echo
+echo -e "${CYAN}${BOLD_TEXT}==============================================${RESET_FORMAT}"
+echo -e "${YELLOW}${BOLD_TEXT}          Subscribe To Arcade Labs          ${RESET_FORMAT}"
+echo -e "${CYAN}${BOLD_TEXT}==============================================${RESET_FORMAT}"
+echo
