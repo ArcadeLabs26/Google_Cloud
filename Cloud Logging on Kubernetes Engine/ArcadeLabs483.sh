d5e4b0ea46a691a69e92acce168ea7dcada2a7ee
Arcade Labs <arcadelabs26@gmail.com>
2025-08-25 04:23:36 +0530

    Add files via upload

---
+++ Cloud Logging on Kubernetes Engine/ArcadeLabs483.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=$(tput bold)
+RESET=$(tput sgr0)
+
+
+echo
+echo -e "${CYAN}${BOLD}================================================${RESET}"
+echo -e "${YELLOW}${BOLD}             Arcade Labs Solution             ${RESET}"
+echo -e "${CYAN}${BOLD}================================================${RESET}"
+echo
+
+
+echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Progress${RESET}"
+
+gcloud auth list
+
+export ZONE=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+export REGION=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-region])")
+
+export PROJECT_ID=$(gcloud config get-value project)
+gcloud config set project $DEVSHELL_PROJECT_ID
+
+gcloud config set compute/zone "$ZONE"
+gcloud config set compute/region "$REGION"
+
+git clone https://github.com/GoogleCloudPlatform/gke-logging-sinks-demo
+
+sleep 10
+
+cd gke-logging-sinks-demo
+
+sleep 10
+
+sed -i 's/  version = "~> 2.11.0"/  version = "~> 2.19.0"/g' terraform/provider.tf
+
+sed -i 's/  filter      = "resource.type = container"/  filter      = "resource.type = k8s_container"/g' terraform/main.tf
+
+make create
+make validate
+
+gcloud logging read "resource.type=k8s_container AND resource.labels.cluster_name=stackdriver-logging" --project=$PROJECT_ID
+
+# run the specific query
+gcloud logging read "resource.type=k8s_container AND resource.labels.cluster_name=stackdriver-logging" --project=$PROJECT_ID --format=json
+
+
+
+gcloud logging sinks create techcps \
+    bigquery.googleapis.com/projects/$PROJECT_ID/datasets/bq_logs \
+    --log-filter='resource.type="k8s_container" 
+resource.labels.cluster_name="stackdriver-logging"' \
+    --include-children \
+    --format='json'
+
+
+sleep 20
+
+
+
+bq query --use_legacy_sql=false \
+"
+SELECT * FROM \`$DEVSHELL_PROJECT_ID.gke_logs_dataset.diagnostic_log_*\`
+WHERE _TABLE_SUFFIX BETWEEN FORMAT_DATE('%Y%m%d', CURRENT_DATE() - INTERVAL 1 DAY) AND FORMAT_DATE('%Y%m%d', CURRENT_DATE()) 
+"
+
+
+
+
+echo "${GREEN}${BOLD}Congratulations:- ${RESET}" "${WHITE}${BOLD}Lab Completed !!!${RESET}"
+
+
+echo
+echo -e "${CYAN}${BOLD}================================================${RESET}"
+echo -e "${YELLOW}${BOLD}           Subscribe To Arcade Labs           ${RESET}"
+echo -e "${CYAN}${BOLD}================================================${RESET}"
+echo
